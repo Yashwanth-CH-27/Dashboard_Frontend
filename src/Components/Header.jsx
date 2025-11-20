ee32fbb879cb81f78cf1351635d3b1606dec6fff
Yash <yaswanth.chundru@gmail.com>
2025-11-20 16:44:20 +0530

    profile edit

--- src/Components/Header.jsx
+++ src/Components/Header.jsx
@@ -41,8 +41,6 @@ const Header = () => {
                 <span className="badge">New</span>
             </Link>
             </li>
-            <li><Link to="/connections">Connections</Link></li>
-            <li><Link to="/requests">Requests</Link></li>
             <li onClick={handleLogout}><a>Logout</a></li>
         </ul>
         </div>
--- src/Components/Profile.jsx
+++ src/Components/Profile.jsx
@@ -1,43 +1,89 @@
-import React from "react";
-import { useSelector } from "react-redux";
+import axios from "axios";
+import { useState } from "react";
+import { useDispatch } from "react-redux";
+import { BASE_URL } from "../utils/constants";
+import { addUser } from "../Redux/userSlice";
 
-const Profile = () => {
-    const user = useSelector((store) => store.user)
-    const {firstName, lastName, age, gender} = user
+const ProfileEdit = ({user}) => {
+  const [firstName, setFirstName] = useState(user.firstName);
+  const [lastName, setLastName] = useState(user.lastName);
+  const [age, setAge] = useState(user.age || "");
+  const [gender, setGender] = useState(user.gender || "");
+  const [errMessage, setErrorMessage] = useState("");
+  const [toastMessage,setToastMessage] = useState(false);
+  const dispatch = useDispatch();
+  const hadleSave = async () => {
+    setErrorMessage("");
+    try{
+        const res = await axios.patch(
+        BASE_URL + "profile/edit",
+        { firstName, lastName, age, gender},
+        { withCredentials: true }
+        );
+        dispatch(addUser(res.data))
+        setToastMessage(true)
+        setTimeout(()=>{
+          setToastMessage(false)
+        },3000)
+    }catch(err){
+        setErrorMessage(err?.response?.data)
+        console.log(err)
+    }
+  };
   return (
-    <div>
-      <div className="hero bg-base-200 min-h-screen">
-        <div className="hero-content flex-col lg:flex-row-reverse">
-          <div className="text-center lg:text-left">
-            <h1 className="text-5xl font-bold">This is your profile data!</h1>
-            <p className="py-6">
-              You can view and edit your profile here.
-            </p>
-          </div>
-          <div className="card bg-base-100 w-full max-w-sm shrink-0 shadow-2xl">
-            <div className="card-body">
-              <fieldset className="fieldset">
-                <label className="label">First Name</label>
-                <input value={firstName} type="text" className="input" />
-                <label className="label">Last Name</label>
-                <input
-                  value={lastName}   
-                  type="text"
-                  className="input"
-                  
-                />
-                <label className="label">Age</label>
-                <input value={age} type="text" className="input" />
-                <label className="label">Gender</label>
-                <input value={gender} type="text" className="input" />
-                <button className="btn btn-neutral mt-4">Login</button>
-              </fieldset>
-            </div>
+    <div className="flex justify-center my-20 gap-30">
+      <div className="card card-dash bg-base-300 w-96">
+        <div className="card-body">
+          <h2 className="card-title">Edit your profile</h2>
+          <fieldset className="fieldset">
+            <legend className="fieldset-legend">FirstName</legend>
+            <input
+              value={firstName}
+              type="text"
+              className="input"
+              onChange={(e) => setFirstName(e.target.value)}
+            />
+          </fieldset>
+          <fieldset className="fieldset">
+            <legend className="fieldset-legend">Last Name</legend>
+            <input
+              value={lastName}
+              type="text"
+              className="input"
+              onChange={(e) => setLastName(e.target.value)}
+            />
+          </fieldset>
+          <fieldset className="fieldset">
+            <legend className="fieldset-legend">Age</legend>
+            <input
+              value={age}
+              type="text"
+              className="input"
+              onChange={(e) => setAge(e.target.value)}
+            />
+          </fieldset>
+          <fieldset className="fieldset">
+            <legend className="fieldset-legend">Gender</legend>
+            <input
+              value={gender}
+              type="text"
+              className="input"
+              onChange={(e) => setGender(e.target.value)}
+            />
+          </fieldset>
+          <div className="card-actions justify-center py-5">
+            <p className="text-red-700">{errMessage}</p>
+            <button className="btn btn-primary" onClick={hadleSave}>Save Profile</button>
           </div>
         </div>
       </div>
+      {toastMessage && <div className="toast toast-top toast-center">
+        <div className="alert alert-success">
+          <span>Edits saved successfully!!!</span>
+        </div>
+      </div>}
     </div>
   );
 };
 
-export default Profile;
+export default ProfileEdit;
