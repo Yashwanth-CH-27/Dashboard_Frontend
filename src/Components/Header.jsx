c43690f2bd55e9c51f14039b1ebf62bdcc6b92ff
Yash <yaswanth.chundru@gmail.com>
2025-11-20 16:24:26 +0530

    Header config

--- src/Components/Header.jsx
+++ src/Components/Header.jsx
@@ -1,13 +1,54 @@
-import React from "react";
+import axios from "axios"
+import { useDispatch, useSelector } from "react-redux"
+import { Link, useNavigate } from "react-router-dom"
+import { BASE_URL } from "../utils/constants"
+import { removeUser } from "../Redux/userSlice"
 
 const Header = () => {
-  return (
-    <div>
-      <div className="navbar bg-neutral text-neutral-content">
-        <button className="btn btn-ghost text-xl">Dashboard</button>
-      </div>
+    const dispatch = useDispatch();
+    const navigate = useNavigate();
+    const handleLogout = async () => {
+        try{
+            await axios.post(BASE_URL + "signOut",{}, {withCredentials:true})
+            dispatch(removeUser())
+            return navigate("/login")
+        }catch(err){
+            console.log(err)
+        }
+    }
+    const user = useSelector(store => store.user)
+    return(
+    <div className="navbar bg-base-300 shadow-sm">
+    <div className="flex-1">
+        <Link to= "/" className="btn btn-ghost text-xl">Dashboard🔥</Link>
     </div>
-  );
-};
+    {user && <div className="flex gap-2">
+        <p className="pt-1.5">Welcome, {user.firstName}</p>
+        <div className="dropdown dropdown-end">
+        <div tabIndex={0} role="button" className="btn btn-ghost btn-circle avatar mx-4">
+            <div className="w-10 rounded-full">
+            <img
+                alt="Tailwind CSS Navbar component"
+                src="https://img.daisyui.com/images/stock/photo-1534528741775-53994a69daeb.webp"/>
+            </div>
+        </div>
+        <ul
+            tabIndex={0}
+            className="menu menu-sm dropdown-content bg-base-100 rounded-box z-1 mt-3 w-52 p-2 shadow">
+            <li>
+            <Link to= "/profile" className="justify-between">
+                Profile
+                <span className="badge">New</span>
+            </Link>
+            </li>
+            <li><Link to="/connections">Connections</Link></li>
+            <li><Link to="/requests">Requests</Link></li>
+            <li onClick={handleLogout}><a>Logout</a></li>
+        </ul>
+        </div>
+    </div>}
+    </div>
+    )
+}
 
-export default Header;
+export default Header
